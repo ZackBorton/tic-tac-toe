ca48e0bb05bf560740b6784fb3733a33aae97d6e
Zack Borton <zaborton@gmail.com>
2020-01-06 20:31:12 -0600

    Further breaking up solution and adding test

--- src/components/Square.test.js
+++ src/components/Square.test.js
@@ -2,9 +2,8 @@ import React from 'react'
 import {render} from '@testing-library/react';
 import Square from './Square'
 
-
-test('returns a square element',()  => {
+test('returns a square element', () => {
     const {getByText} = render(<Square/>);
     // const linkElement = getByText(/Tic Tac Toe/i);
     expect(true).toBe(true);
-})
+});
